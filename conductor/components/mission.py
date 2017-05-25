449604684d8fdddaf55078949fc4153c3d4a617e
marvin <marvinroche14@gmail.com>
2017-05-25 12:07:11 +0200

    rangement dossiers

--- conductor/components/mission.py
+++
@@ -1,8 +0,0 @@
-#import du modele permettant de recuperer les livraisons
-
-from Server.WebApp_ORM.drone.models import Delivery
-
-class Mission:
-    def __init__(self):
-        lstAllMissions = Delivery.objects.all()
-
