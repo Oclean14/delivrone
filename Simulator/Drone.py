fe341eea3dffa340663a6a28c2b244abbe46b0c5
raj <radjasegar.raj@gmail.com>
2017-07-03 01:56:51 +0200

    1ere version Delivery creation random

--- Simulator/Drone.py
+++ Simulator/Drone.py
@@ -1,111 +1,150 @@
 # -*- coding: utf-8 -*-
 import math, random
+import sqlite3
 import time
 from threading import Thread
-from Battery import Battery
-#from Simulator import main
+from Simulator.Battery import Battery
+from Simulator import main
 
 class Drone(Thread) :
-	
-	drone_status = {
-		0 : "free in warehouse",
-		1 : "flying, executing mission",
-		2 : "Taking packet in warehouse",
-		3 : "Waiting for battery changement",
-		4 : "Changing battery",
-		5 : "Free flying",
-		6 : "In maintenance",
-		7 : "Delivering"
-	}
-	
-	"""
-		ctor
-	
-	def __init__(self, homeLocation, position, failureFrequency, averageSpeed, battery):
-		Thread.__init__(self)
-		self.homeLocation = homeLocation 
-		self.position = position
-		self.status = "free"
-		self.failureFrequency = failureFrequency
-		self.averageSpeed = averageSpeed
-		self.battery = battery
-	"""
-	__id = 0
-	def __init__(self, position, averageSpeed):
-		Thread.__init__(self)
-		Drone.__id += 1
-		self.id = Drone.__id
-		self.position = position
-		self.status = 1
-		self.startingPosition = position
-		self.averageSpeed = averageSpeed
-		self.battery = Battery(300, 100, 2)
-		
-	#	Toute la logique du drone
-	#	- Perte de batterie
-	#	- Deplacement
-	def run(self):
-		while not(self.isOnTopOfDirection()) and self.status == 1:
-			print("I'm at", self.position, " and I'm going to ", self.direction, " i have battery level of ", self.battery.chargePercentage)
-			distance = math.sqrt(self.square(self.direction[0] - self.startingPosition[0]) + self.square(self.direction[1] - self.startingPosition[1]))
-			speedVector = [(self.averageSpeed * (self.direction[0] - self.startingPosition[0])/distance), (self.averageSpeed * (self.direction[1] - self.startingPosition[1])/distance)]
-			self.position[0] = self.position[0] + speedVector[0]
-			self.position[1] = self.position[1] + speedVector[1]
-			self.battery.use();
-			if self.battery.chargePercentage <= 0:
-				self.status = "noenergy"
-			time.sleep(0.1)
-		
 
-	# TODO: ajouter le commentaire de la fonction setDirection
-	def setDirection(self, direction):	
-		self.direction = direction
+    drone_status = {
+        0 : "free in warehouse",
+        1 : "flying, executing mission",
+        2 : "Taking packet in warehouse",
+        3 : "Waiting for battery changement",
+        4 : "Changing battery",
+        5 : "Free flying",
+        6 : "In maintenance",
+        7 : "Delivering"
+    }
 
-	##
-	#	Ajouter une nouvelle mission a effectuer
-	def addDelivery(self,delivery):
-		self.deliveryList.append(delivery)
+    global django_status;
+    django_status = [
+        "0",
+        "UNINIT",
+        "BOOT",
+        "CALIBRATING",
+        "STANDBY",
+        "ACTIVE",
+        "CRITICAL",
+        "EMERGENCY",
+        "POWEROFF",
+    ]
 
-	#TODO: ajouter le commentaire de la fonction executeDelivery
-	def executeDelivery(self, delivery):
-		for pos in delivery:
-			i = 1
-		
-	##
-	#	Recupere le niveau de charge du drone
-	def getChargeLevel(self):
-		return self.battery.lvl
+    """
+        ctor
 
-	##
-	#	Recupere l'etat du drone
-	def getStatus(self):
-		return self.status
+    def __init__(self, homeLocation, position, failureFrequency, averageSpeed, battery):
+        Thread.__init__(self)
+        self.homeLocation = homeLocation
+        self.position = position
+        self.status = "free"
+        self.failureFrequency = failureFrequency
+        self.averageSpeed = averageSpeed
+        self.battery = battery
+    """
+    __id = 0
+    def __init__(self, position, averageSpeed):
+        Thread.__init__(self)
+        Drone.__id += 1
+        self.id = Drone.__id
+        self.position = position
+        self.status = 1
+        self.startingPosition = position
+        self.averageSpeed = averageSpeed
+        self.battery = Battery(300, 100, 2)
 
-	##
-	#	Renseigne l'etat du drone
-	def setStatus(self, status):
-		self.status = status
-	##	
-	# Recupere la direction du drone
-	def getDirection(self):
-		return self.direction
-	##	
-	# Suit les points fournit par un eventuelle calculateur de vol
-	def followPoints(self):
-		return
+    #	Toute la logique du drone
+    #	- Perte de batterie
+    #	- Deplacement
+    def run(self):
+        while not(self.isOnTopOfDirection()) and self.status == 1:
+            print("I'm at", self.position, " and I'm going to ", self.direction, " i have battery level of ", self.battery.chargePercentage)
+            distance = math.sqrt(self.square(self.direction[0] - self.startingPosition[0]) + self.square(self.direction[1] - self.startingPosition[1]))
+            speedVector = [(self.averageSpeed * (self.direction[0] - self.startingPosition[0])/distance), (self.averageSpeed * (self.direction[1] - self.startingPosition[1])/distance)]
+            self.position[0] = self.position[0] + speedVector[0]
+            self.position[1] = self.position[1] + speedVector[1]
+            self.battery.use();
+            if self.battery.chargePercentage <= 0:
+                self.status = "noenergy"
+            time.sleep(0.1)
 
-	def square(self,x):
-		return x*x
 
-	# TODO: ajouter le commentaire de la fonction
-	def isOnTopOfDirection(self):
-		distance = math.sqrt(self.square(self.direction[0] - self.position[0]) + self.square(self.direction[1] - self.position[1]))
-		print(distance)
-		if(distance > 3):
-			return False
-		else:
-			return True
+    # TODO: ajouter le commentaire de la fonction setDirection
+    def setDirection(self, direction):
+        self.direction = direction
 
-	def subscribeToStation(self, station):
-		self.status = 4
-		station.droneQueue.append(self)
+    ##
+    #	Ajouter une nouvelle mission a effectuer
+    def addDelivery(self,delivery):
+        self.deliveryList.append(delivery)
+
+    #TODO: ajouter le commentaire de la fonction executeDelivery
+    def executeDelivery(self, delivery):
+        for pos in delivery:
+            i = 1
+
+    @classmethod
+    def FindIdByStatus(self, status):
+        status = django_status.index(status);
+        status = str(status);
+        conn = sqlite3.connect('..\Server\WebApp_ORM\drone.db')
+        cursor = conn.cursor()
+        cursor.execute("""SELECT id FROM Drone WHERE status=\'""" + status + """\'""")
+        packets = cursor.fetchall()
+        conn.close()
+        return packets
+
+    @classmethod
+    def UpdateStatusByID(self, id, status):
+        status =django_status.index(status);
+        status = str(status);
+        conn = sqlite3.connect('..\Server\WebApp_ORM\drone.db')
+        print("Opened database successfully");
+        cursor = conn.cursor()
+        # cursor.execute("UPDATE Delivery SET status=\'"+ status + "\' WHERE id=\'" + id + "\'");
+        cursor.execute("UPDATE Drone SET status=? WHERE id=?", (status, id));
+        conn.commit()
+        cursor.close()
+        print("Operation done successfully");
+
+    ##
+    #	Recupere le niveau de charge du drone
+    def getChargeLevel(self):
+        return self.battery.lvl
+
+    ##
+    #	Recupere l'etat du drone
+    def getStatus(self):
+        return self.status
+
+    ##
+    #	Renseigne l'etat du drone
+    def setStatus(self, status):
+        self.status = status
+    ##
+    # Recupere la direction du drone
+    def getDirection(self):
+        return self.direction
+    ##
+    # Suit les points fournit par un eventuelle calculateur de vol
+    def followPoints(self):
+        return
+
+    def square(self,x):
+        return x*x
+
+    # TODO: ajouter le commentaire de la fonction
+    def isOnTopOfDirection(self):
+        distance = math.sqrt(self.square(self.direction[0] - self.position[0]) + self.square(self.direction[1] - self.position[1]))
+        print(distance)
+        if(distance > 3):
+            return False
+        else:
+            return True
+
+    def subscribeToStation(self, station):
+        self.status = 4
+        station.droneQueue.append(self)
+
--- Simulator/Scheduler.py
+++ Simulator/Scheduler.py
@@ -1,5 +1,10 @@
 from time import sleep
 from Simulator.Delivery import *
+from Simulator.Drone import *
+from Simulator.Packet import *
+
+global i;
+i = 6;
 
 class Scheduler() :
 
@@ -10,4 +15,29 @@ class Scheduler() :
         return mission
 
     def randomAssignMission (self, frequency) :
-        print("HELLO WORLD")
+        time = 1
+        if frequency != time :
+                sleep(time * frequency)
+        else :
+                sleep(time)
+        packets = Packet.FindIdByStatus("Waiting");
+        print(packets)
+        packetList = []
+        for packet in packets:
+            print(packet[0])
+            i = i + 1;
+            j = str(i);
+            print("for packet id :" + str(packet[0]) + " we are going to create the delivery : delivery" + j)
+            print("We will first search a free drone :")
+            drone_id = "0";
+            drones = Drone.FindIdByStatus("STANDBY")
+            for drone in drones:
+                drone_id = drone[0]
+                drone_id = str(drone_id);
+                print("we are going to assign this mission to drone id :" + str(drone_id))
+                break
+
+            print("VOILA LE DRONE ID " + drone_id)
+            mission = Delivery("delivery" + j, "55", drone_id, "2", str(packet[0]), "NOT STARTED");
+            mission.save()
+            drone = Drone.UpdateStatusByID(drone_id, "ACTIVE")
