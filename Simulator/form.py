f9b8eb306a476448b9839119bf31fc3274861085
Evy SIMION <evy.simion@gmail.com>
2017-07-12 02:43:05 +0200

    Restructuration du formulaire

--- Simulator/form.py
+++ Simulator/form.py
@@ -1,9 +1,13 @@
 try:
+    #python2
     import Tkinter as tk
     import ttk
+    import tkFont as tkfont
 except ImportError:
+    #python3
     import tkinter as tk
     import tkinter.ttk as ttk
+    from tkinter import font  as tkfont
 
 class Base_Form(object):
     """Base class of all forms"""
@@ -24,12 +28,6 @@ class Base_Form(object):
 
 class Base_SubmitButton(object):
     """Base class of submit buttons"""
-
-    def __str__(self):
-        sb = []
-        for key in self.__dict__:
-            sb.append("{key}='{value}'".format(key=key, value=self.__dict__[key])) 
-        return ', '.join(sb)
 
     def submit(self):
         form_widget = self
@@ -81,8 +79,8 @@ class Base_SubmitButton(object):
                 continue
 
             form_data[field_name] = field_value
-            form_data["WarehouseList"] = CoordonneesWH
-            form_data["stationList"] = CoordonneesDS
+            form_data["WarehouseList"] = app.frames["PageTwo"].CoordonneesWH
+            form_data["stationList"] = app.frames["PageTwo"].CoordonneesDS
 	
 	form_data = str(form_data)	
 	form_data = re.sub(r"(\\')", "'", form_data)
@@ -125,6 +123,137 @@ class Submit_TButton(ttk.Button, Base_SubmitButton):
         kw["command"] = self.submit
         ttk.Button.__init__(self, parent, *args, **kw)
 
+class SampleApp(tk.Tk):
+
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
+
+        self.title_font = tkfont.Font(family='Helvetica', size=18, weight="bold", slant="italic")
+
+        # the container is where we'll stack a bunch of frames
+        # on top of each other, then the one we want visible
+        # will be raised above the others
+        container = tk.Frame(self)
+        container.pack(side="top", fill="both", expand=True)
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+
+        self.frames = {}
+        for F in (PageOne, PageTwo):
+            page_name = F.__name__
+            frame = F(parent=container, controller=self)
+            self.frames[page_name] = frame
+
+            # put all of the pages in the same location;
+            # the one on the top of the stacking order
+            # will be the one that is visible.
+            frame.grid(row=0, column=0, sticky="nsew")
+
+        self.show_frame("PageOne")
+
+    def show_frame(self, page_name):
+        '''Show a frame for the given page name'''
+        frame = self.frames[page_name]
+        frame.tkraise()
+
+class PageOne(tk.Frame):
+
+    def myField(self, form, myinput, nbRow):
+	tk.Label(form,text=myinput).grid(row=nbRow,column=0,sticky=E,pady=(8,0))
+	entry = tk.Entry(form)
+        entry.fieldname = myinput
+        entry.grid(row=nbRow,column=1,sticky= E+W)
+
+    def scriptMain(self):
+        os.system('python main.py')
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+	label = tk.Label(self, text="Delivrone Configuration 1/2", font=controller.title_font)
+        label.pack(side="top", fill="x", pady=10)
+
+	fields = ["aerianspace", "numberDrone", "averageSpeed", "failureFrequency", "numberStation", "batteriesPerStation", "chargingTime", "changingTime", "mode", "deliveryFrequency", "consumption", "maxCycle", "startAt", "startAt", "motorsNumber", "motorPrice", "propellersNumber", "propellerPrice", "droneList"]
+        count = 1
+	pp = pprint.PrettyPrinter(indent=4)
+
+        #self.title('Initialization')
+        self.form = Form(self, action =lambda data: messagebox.showinfo("form data",pp.pformat(data)))
+        self.form.pack(expand=True, fill="both", ipadx=10, ipady=10)
+
+        for field in fields:
+            self.myField(self.form, field, count)
+            count = count + 1	
+
+	Submit_Button(self.form, text="Submit").grid(row=count,column=1,sticky =E)
+        tk.Label(self, text="BEFORE SUBMIT").pack()
+	tk.Button(self, text ='Launch Simulator', command = self.scriptMain).pack(side=RIGHT,padx = 5,pady = 5)
+	button = tk.Button(self, text="Go to WareHouse & Station Edition",
+                           command=lambda: controller.show_frame("PageTwo"))
+        button.pack(side=RIGHT,padx = 5,pady = 5)
+
+class PageTwo(tk.Frame):
+    
+    def ClicWH(self, event):
+        X = event.x
+        Y = event.y
+        r = 10
+        self.CoordonneesWH.append("{'longitude':'" + str(X) + "','latitude':'" + str(Y) + "'}")
+        self.Canevas.create_rectangle(X-r, Y-r, X+r, Y+r, outline='black',fill='blue', tags='WH')
+        print str(self.CoordonneesWH)
+
+    def ClicDS(self, event):
+        X = event.x
+        Y = event.y
+        r = 10
+        self.CoordonneesDS.append("{'longitude':'" + str(X) + "','latitude':'" + str(Y) + "'}")
+        self.Canevas.create_rectangle(X-r, Y-r, X+r, Y+r, outline='black',fill='red', tags='DS')
+        print str(self.CoordonneesDS)
+
+    def EffacerWH(self):
+        # Efface la zone graphique 
+        self.CoordonneesWH[:] = []
+        for WH in self.Canevas.find_withtag("WH"):
+            self.Canevas.delete(WH)
+
+    def EffacerDS(self):
+        self.CoordonneesDS[:] = []
+        for DS in self.Canevas.find_withtag("DS"):
+            self.Canevas.delete(DS)
+
+    def EditWH(self):
+        self.Canevas.bind('<Button-1>',ClicWH)
+
+    def EditDS(self):
+        self.Canevas.bind('<Button-1>',ClicDS)
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        label = tk.Label(self, text="Delivrone Configuration 2/2", font=controller.title_font)
+        label.pack(side="top", fill="x", pady=10)
+
+	Largeur = 680
+	Hauteur = 520
+	self.CoordonneesWH = []
+	self.CoordonneesDS = []
+
+	tk.Label(self, text="Click on this window to place Warehouses and Drone stations").pack(anchor=W, padx=(2,0))
+	self.Canevas = tk.Canvas(self, width = Largeur, height =Hauteur, bg ='white')
+	self.Canevas.bind('<Button-1>',self.ClicWH)
+
+	self.Canevas.pack(padx =5, pady =5)
+	tk.Button(self, text ='Edit WareHouses', command = self.EditWH).pack(side=LEFT,padx = 5,pady = 5)
+	tk.Button(self, text ='Edit Stations', command = self.EditDS).pack(side=LEFT,padx = 5,pady = 5)
+	tk.Button(self, text ='Delete Warehouses', command = self.EffacerWH).pack(side=LEFT,padx = 5,pady = 5)
+	tk.Button(self, text ='Delete Stations', command = self.EffacerDS).pack(side=LEFT,padx = 5,pady = 5)
+	#tk.Button(self, text ='Quitter', command = self.destroy).pack(side=LEFT,padx=5,pady=5)
+	#Submit_Button(SampleApp().frames["PageOne"].form, text="Submit").grid(row=count,column=1,sticky =E)
+
+        button = tk.Button(self, text="Go Back to SUBMIT",
+                           command=lambda: controller.show_frame("PageOne"))
+        button.pack(side=LEFT,padx=5,pady=5)
+
 if __name__== "__main__":
     try:
         from Tkinter import *
@@ -138,8 +267,9 @@ if __name__== "__main__":
 
     import pprint
     import re
+    import os
 
-    def myField(form, myinput, nbRow):
+    """ def myField(form, myinput, nbRow):
 	Label(form,text=myinput).grid(row=nbRow,column=0,sticky=E,pady=(8,0))
 	entry = Entry(form)
         entry.fieldname = myinput
@@ -174,7 +304,7 @@ if __name__== "__main__":
 	for DS in Canevas.find_withtag("DS"):
             Canevas.delete(DS)
 
-    def EditWH(val):
+    def EditWH():
         Canevas.bind('<Button-1>',ClicWH)
 
     def EditDS():
@@ -213,4 +343,7 @@ if __name__== "__main__":
 
     Submit_Button(form, text="Submit").grid(row=count,column=1,sticky =E)
 
-    root.mainloop()
+    root.mainloop()"""
+
+    app = SampleApp()
+    app.mainloop()
